1d47873eed3bc5a413ea60f36d6e667b8f2e4bf7
Neinno <34651169+Neinno@users.noreply.github.com>
2018-07-11 14:38:28 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 require ('../private/smarty/Smarty.class.php');
 require ('../private/model.php');
 require ('../private/controller.php');
@@ -16,17 +18,50 @@ $pageno = isset($_GET['pageno']) ? $_GET['pageno'] : '1';
 $searchterm = isset($_GET['searchterm']) ? '%' . $_GET['searchterm'] . '%' : '%';
 
 
-if (isset($_POST['submit_login'])) {
-    login_action();
-    exit();
-}
-
-if (isset($_SESSION['loggedin'])) {
+if (isset($_POST['submit_edit'])) {
+    submit_edit_action();
     cms_action();
     exit();
 }
 
+if (isset($_POST['submit_login'])) {
+    login_action();
+}
+
+if (isset($_POST['add_submit'])) {
+    addarticle_action();
+    exit();
+}
+
+
+if (isset($_SESSION['loggedin'])) {
+    check_cookie();
+    cms_action();
+    exit();
+}
+
+if (isset($_POST['logout'])) {
+    logout_action();
+    news_action();
+    exit();
+}
+
+if (isset($_POST['add'])) {
+    add_action();
+    exit();
+}
+
+if (isset($_POST['delete_submit'])) {
+    delete_action();
+    cms_action();
+    exit();
+}
+
+
 switch ($page) {
+    case 'about': about_action(); break;
+    case 'add': add_action(); break;
+    case 'edit': edit_action(); break;
     case 'admin': admin_action(); break;
     case 'cms': cms_action(); break;
     case 'home': homepage_action($smarty); break;
